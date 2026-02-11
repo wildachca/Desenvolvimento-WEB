8f3e4ea6bff53d1c14f8fc80a47f6ae13070c555
Wildachca Jean <wildachcadada@gmail.com>
2026-02-11 17:21:54 -0300

    Node

---
+++ Node.js/app.js
@@ -0,0 +1,9 @@
+const op = require('./operacoes');
+
+const n1 = 10;
+const n2 = 5;
+
+console.log("Soma:", op.somar(n1, n2));
+console.log("Subtração:", op.subtrair(n1, n2));
+console.log("Multiplicação:", op.multiplicar(n1, n2));
+console.log("Divisão:", op.dividir(n1, n2));
---
+++ Node.js/calculadora.js
@@ -0,0 +1,12 @@
+function somar(a,b){
+    return a + b;
+}
+
+function subtrair(a,b){
+    return a - b;
+}
+
+module.exports = {
+    somar,
+    subtrair
+};
